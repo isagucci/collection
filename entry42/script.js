4832c8ad8b4e7cb8bb46a4379805aaf9ab156330
isagucci <gucci004@newschool.edu>
2024-05-08 22:53:35 -0400

    review

--- entry42/script.js
+++ entry42/script.js
@@ -1,7 +1,9 @@
 function hideImage() {
     var image = document.querySelector('.main');
     image.style.display = 'none';
+    var clickSound = document.getElementById('clickSound');
+    clickSound.play();
 }
 
 let trigger = document.querySelector("img.main");
-trigger.addEventListener("click",hideImage);
+trigger.addEventListener("click", hideImage);
--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+window.addEventListener('load', function () {
+    var loadingScreen = document.querySelector('.loading-screen');
+    var gif = loadingScreen.querySelector('img');
+
+    loadingScreen.addEventListener('animationend', function() {
+      loadingScreen.style.display = 'none';
+    });
+    setTimeout(function() {
+      loadingScreen.style.animation = 'loadingAnimation 4s ease-in-out forwards';
+    }, 2000); 
+  
+    
+    setTimeout(function() {
+      loadingScreen.style.display = 'none';
+    }, 10000); 
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var boxes = document.querySelectorAll('.box');
+
+    // Loop through each box element
+    boxes.forEach(function(box) {
+        // Select the <a> tag within the current box
+        var link = box.querySelector('a');
+
+        // Add event listener for mouseover
+        box.addEventListener('mouseover', function() {
+            // Change color of <a> tag to white
+            link.style.color = 'white';
+        });
+
+        // Add event listener for mouseout (to revert the color)
+        box.addEventListener('mouseout', function() {
+            // Revert color of <a> tag to original color
+            link.style.color = ''; // This will revert to the default color defined in CSS
+        });
+    });
+});
+
+
+  
 function filterItems() {
     const checkboxes = document.querySelectorAll('.checkbox');
     const items = document.querySelectorAll('.box');
